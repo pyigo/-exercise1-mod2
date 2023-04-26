a9a99121f3d9106f212d0111438db2c2f001b0a4
paulette <paulette.yigo@gmail.com>
2023-04-26 12:50:54 -0400

    calculated total

---
+++ tips.js
@@ -0,0 +1,10 @@
+let foodCost = 79.25;
+let tax = 6.54;
+let tip = 12.00;
+
+// Calculations
+var totalDue= foodCost + tax + tip;
+console.log(`your total is ${totalDue}`);
+console.log("Food cost is " + foodCost + "and tax is " + tax );
+console.log("Tip is " + tip.toFixed(2));
+console.log(  "Total Due is " + totalDue)

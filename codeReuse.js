4d32ac42db2f1bc4f569488b3e60636120fc96fc
paulette <paulette.yigo@gmail.com>
2023-04-26 13:50:23 -0400

    created reuse file

---
+++ codeReuse.js
@@ -0,0 +1,11 @@
+// COMPLEX DATA TYPES - COLLECTIONS - MULTIPLE VALUES
+students = ["Kamila", "Khayra", "Axelle"] //an array(aka an ordered  List) of Strings 
+address = {// Create an object (Like a block of data or Dictionary//
+    line1 : "1750 6th st", // line1 is the key, 1750 is the value pair
+    city : "Columbia city",
+    state : "NJ",
+    zip : "07103" //we have xreated and address object.
+} 
+
+console.log(students[0]) //will print Kamila.   0 is the offset from starting from left
+console.log(address.zip)

3abfa01309ebc8d433e05c61c316bfe26b3e2424
paulette <paulette.yigo@gmail.com>
2023-04-26 13:29:32 -0400

    added files

---
+++ conditional.js
@@ -0,0 +1,12 @@
+let hour = 10; //0-23
+if(hour<= 10) // logical Boolean(true/false) data type expression
+{
+console.log("Good morning");
+
+}else if (hour <17){
+
+console.log("Good afternoon");
+}else{
+   // NO NEDD TO CHECK IS AS IF ITS MOR ING AND NOT AFTERNOON IT MUST BE EVENING
+   console.log("GOOD EVENING")
+}
